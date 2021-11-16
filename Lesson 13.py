bed0e14c4a7dc13d9974272ad9ad07815674dcc7
Frdm1ua <alenoxs96@gmail.com>
2021-11-16 21:22:22 +0200

    final

--- Lesson 13.py
+++ Lesson 13.py
@@ -1,18 +1,18 @@
 def arithmetic(a, b, c):
     if c == '+':
-        print(a + b)
+        return(a + b)
     elif c == '-':
-        print(a - b)
+        return(a - b)
     elif c == '*':
-        print(a * b)
+        return(a * b)
     elif c == '/':
-        print(a / b)
+        return(a / b)
     else:
-        print("Незвестная операция")
+        return("Незвестная операция")
 
 
 a = int(input('a: '))
 b = int(input('b: '))
 c = input('Действие(+, -, *, / ): ')
 
-arithmetic(a, b, c)
+print(arithmetic(a, b, c))
